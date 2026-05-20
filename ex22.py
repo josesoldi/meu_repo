009093122df8042e6f70a8b85a46e100aa45c310
josesoldi <joseotavionunessoldi@usp.br>
2026-05-19 22:16:10 -0300

    Change plot function from cubic to sine

--- ex22.py
+++ ex22.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-x = np.linspace (-100, 100, 100)
-y = x**3 + 2 * x
+x = np.linspace (-10, 10, 100)
+y = np.sin(x)
 
 plt.title ('Gráfico')
 plt.xlabel ('Eixo X')
 plt.ylabel ('Eixo Y')
 plt.grid (True)
-plt.plot (x, y, label = 'f(x) = x³ + 2x')
+plt.plot (x, y, label = 'f(x) = seno(x)')
 plt.legend()
 plt.show()
